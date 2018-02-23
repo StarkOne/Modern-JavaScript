272ea5222ae3c395eeedade4602df5d66c6b4443
StarkOne <Stark.pro40@gmail.com>
2018-02-23 14:45:56 +0700

    Fetch With Async Await

---
+++ Custom-HTTP-Library3 (Fetch With Async Await)/app.js
@@ -0,0 +1,31 @@
+const http = new EaseHTTP;
+
+//get
+// const users = http.get("http://jsonplaceholder.typicode.com/users")
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+
+//post 
+// User data
+const data = {
+  name: "Vlad",
+  username: "PRO",
+  email: "StarkPso@april.biz"
+};
+
+// Create User
+// http.post("http://jsonplaceholder.typicode.com/users", data)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+
+//Updata POST
+// http.put("http://jsonplaceholder.typicode.com/users/2", data)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+
+// http.delete("http://jsonplaceholder.typicode.com/users/2", data)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
